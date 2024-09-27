0bba14cca3eb021a12d71514728dde5f6c6fdb47
Zloybanan4ik <denik55557777809@gmail.com>
2024-09-27 11:33:20 +0300

    Version 5: Added conditional logic to comment on long names

--- 123.py
+++ 123.py
@@ -1,7 +1,11 @@
-# Version 4
+# Version 5
 greetings = ["Hi", "Hello", "Hey", "Greetings", "Salutations"]
 
 for _ in range(3):
     name = input("Enter your name: ")
     greeting = greetings[_ % len(greetings)]
-    print(f"{greeting}, {name}!")
+    
+    if len(name) > 5:
+        print(f"{greeting}, {name}! You have a long name!")
+    else:
+        print(f"{greeting}, {name}!")
