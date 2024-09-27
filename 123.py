60d42b2628eb9d11842e57f65ef4d75322c26676
Zloybanan4ik <denik55557777809@gmail.com>
2024-09-27 11:35:11 +0300

    Version 7: Added time-based greeting based on current hour

--- 123.py
+++ 123.py
@@ -1,13 +1,24 @@
-# Version 6
+# Version 7
 import random
+from datetime import datetime
 
 greetings = ["Hi", "Hello", "Hey", "Greetings", "Salutations"]
 
+def get_time_based_greeting():
+    current_hour = datetime.now().hour
+    if current_hour < 12:
+        return "Good morning"
+    elif 12 <= current_hour < 18:
+        return "Good afternoon"
+    else:
+        return "Good evening"
+
 for _ in range(3):
     name = input("Enter your name: ")
-    greeting = random.choice(greetings)
+    random_greeting = random.choice(greetings)
+    time_based_greeting = get_time_based_greeting()
     
     if len(name) > 5:
-        print(f"{greeting}, {name}! You have a long name!")
+        print(f"{time_based_greeting}, {name}! {random_greeting}! You have a long name!")
     else:
-        print(f"{greeting}, {name}!")
+        print(f"{time_based_greeting}, {name}! {random_greeting}!")
