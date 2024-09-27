cc6d30a3cdb326299049e9375e8c6ff2c4b8cbb5
Zloybanan4ik <denik55557777809@gmail.com>
2024-09-27 11:34:30 +0300

    Version 6: Randomized greeting selection using random module

--- 123.py
+++ 123.py
@@ -1,9 +1,11 @@
-# Version 5
+# Version 6
+import random
+
 greetings = ["Hi", "Hello", "Hey", "Greetings", "Salutations"]
 
 for _ in range(3):
     name = input("Enter your name: ")
-    greeting = greetings[_ % len(greetings)]
+    greeting = random.choice(greetings)
     
     if len(name) > 5:
         print(f"{greeting}, {name}! You have a long name!")
